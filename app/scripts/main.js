fe1cca584c3251180b2e3aeb82529e4ea7a32124
Michael Kuehn <mkuehn10@gmail.com>
2016-08-09 17:42:37 -0400

    weather api working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,28 +13,37 @@ $(function() {
     var ViewModel = function() {
         var self = this;
 
+        self.lat = '';
+        self.lon = '';
+
+        self.city = ko.observable('');
+
+        self.showTemp = ko.observable(false);
+
         self.kelvinTemp = ko.observable('');
         self.fahrenheit = ko.observable(true);
 
+        self.iconURL = ko.observable('');
+        self.description = ko.observable('');
+
         self.celsiusTemp = function() {
-            return Math.round(kelvinToCelsius(self.kelvinTemp()));
+            return Math.round(kelvinToCelsius(self.kelvinTemp())) + ' °C';
         };
 
         self.fahrenheitTemp = function() {
-            return Math.round(celsiusToFahrenheit(self.celsiusTemp()));
+            return Math.round(celsiusToFahrenheit(kelvinToCelsius(self.kelvinTemp()))) + ' °F';
         };
 
         self.toggleTemp = function() {
             self.fahrenheit(self.fahrenheit() ? false : true);
         }
 
-        console.log(self);
-
         self.getWeather = function() {
             var params = {
-                lat: '38.4225153',
-                lon: '-81.7841701',
-                APPID: APIKey
+                lat: self.lat,
+                lon: self.lon,
+                APPID: APIKey //,
+                //units: 'metric'
             };
             $.ajax({
                     url: 'http://api.openweathermap.org/data/2.5/weather',
@@ -43,13 +52,34 @@ $(function() {
                 .done(function(data) {
                     console.dir(data);
                     self.kelvinTemp(data.main.temp);
-                    //console.log(kelvinToCelsius(data.main.temp));
-                    //console.log(celsiusToFahrenheit(kelvinToCelsius(data.main.temp)));
+                    self.city(data.name + ', ' + data.sys.country);
+                    self.showTemp(true);
+
+                    self.iconURL('http://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
+                    self.description(data.weather[0].description);
                 });
 
         };
 
-        self.getWeather();
+        self.showPosition = function(position) {
+            self.lat = position.coords.latitude;
+            self.lon = position.coords.longitude;
+
+        };
+
+        self.getLocation = function() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(self.showPosition);
+            } else {
+                // x.innerHTML = "Geolocation is not supported by this browser.";
+                console.log('Geolocation is not supported by this browser.');
+            }
+            setTimeout(self.getWeather, 500);
+        };
+
+        self.getLocation();
+
+
     }
 
 
